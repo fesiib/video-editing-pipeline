6bcc8bb19eb718a79d22ecc9559f3a1649dc1c92
Bekzat <bekzat@kaist.ac.kr>
2023-08-29 14:31:18 +0900

    server side fix to reset data after api calls

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from backend.intent_parser import *
 import json
+
 app = Flask(__name__)
-CORS(app)
+
+CORS(app, origins = ["http://localhost:3000"])
+
 '''
 Server side for video editing pipline 
 '''
-intent_parser = IntentParser()
+intent_parser = IntentParser(40)
 '''
 Input:
     Video Reference: youtube link
@@ -16,9 +19,12 @@ Input:
 @app.route("/intent", methods=['GET', 'POST'])
 def parse_intent():
     edit_request = request.json
+    
+    intent_parser.reset()
     edit_response = intent_parser.process_message(edit_request)
-
+    
     response = "User Request: {} \n \n Edit Disambiguation: {}".format(edit_request, edit_response)
+
     return jsonify(edit_response)
 
 '''
@@ -27,6 +33,25 @@ Summary of the edit description
 @app.route("/summary", methods=['GET', 'POST'])
 def fetch_summary():
     data = request.json
-    summary_request = "Generate a several word caption to summarize the purpose of the following video edit request."
-    response = intent_parser.completion_endpoint(summary_request, data.get("input"))
+
+    intent_parser.reset()
+    response = intent_parser.get_summary(data.get("input"))
+
     return jsonify({"summary": response})
+
+def launch_server():
+    app.run(host="0.0.0.0", port=9888)
+
+def test(edit_request):
+    intent_parser.reset()
+    # print("!!!response!!!", json.dumps(intent_parser.process_message({
+    #     "requestParameters": {
+    #         "text": edit_request,
+    #         "editOperation": "text",
+    #     },
+    #     "edits": [],
+    # }), indent=1))
+
+if __name__ == "__main__":
+    launch_server()
+    #test("whenever the person mentions the surface go, emphasize the screen response time")
