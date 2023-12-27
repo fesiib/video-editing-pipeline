31f3b04001ff9c02a70690ace01c0ae8cae9d3be
Bekzat <tlekbay.b@gmail.com>
2023-12-27 19:24:34 +0900

    rename folders

--- analysis/analysis_helpers.py
+++ analysis/analysis_helpers.py
@@ -1,4 +1,19 @@
+def sum(arr):
+    sum = 0
+    for i in arr:
+        sum += i
+    return sum
 
+def avg_std(arr):
+    if len(arr) == 0:
+        return 0, 0
+
+    avg = sum(arr) / len(arr)
+    std = 0
+    for i in arr:
+        std += (i - avg) ** 2
+    std = (std / len(arr)) ** 0.5
+    return avg, std
 
 def combine_dicts(dict1, dict2):
     combined_dict = {}
